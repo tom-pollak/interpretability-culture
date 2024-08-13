2a633b1a979b5ed69a2d90253d31f5aaa1b16954
Tom Pollak <tompollak1000@gmail.com>
2024-08-13 11:23:38 +0100

    prediction

--- interp/sae.py
+++ interp/sae.py
@@ -1,6 +1,7 @@
 # %%
 from interp import *
 from interp.culture import *
+from interp.grid_tokenizer import *
 
 import torch as t
 
@@ -10,60 +11,49 @@ device = get_device()
 
 
 # %%
-hts = load_hooked()
+model: HookedTransformer = load_hooked(0).eval().to(device)  # type: ignore
 qm = load_quizzes()
-
-model: HookedTransformer = hts[0].eval().to(device)  # type: ignore
 
 # %%
 gen_test_w_quiz_(model, qm, n=100)
 dataset, from_w = qm.data_input(model, split="test")
+dataset = dataset.to(device)
 dataset.shape
 
 # %%
-# out = model.generate(
-#     dataset[:1, :5],
-#     max_new_tokens=(100),
-#     stop_at_eos=False,
-# )
-# print(model.tokenizer.decode(out[0]))
+
+out = model.generate('0 ' + model.tokenizer.decode(dataset[2, :304]),
+                        max_new_tokens=(99),
+                        stop_at_eos=False,
+                        temperature=1)
+
+toks = model.tokenizer.encode(out, add_special_tokens=False)
+print(repr_grid(toks))
 
 # %%
-from transformer_lens.utils import test_prompt
 
-# Test the model with a prompt
-test_prompt(
-    model.tokenizer.decode(dataset[0, :303], skip_special_tokens=False),
-    "1",
-    model,
-    prepend_space_to_answer=False,
-)
+idx = t.randint(dataset.size(0), (1,)).item()
+dataset_subset = dataset[idx][None].clone()
+print("from world:", from_w[idx].item())
 
-# %%
-# qm = load_quizzes()
-# qm.problem.save_some_examples(INTERP_RESULTS_DIR)
+problem_struct = qm.problem.get_structure(dataset_subset)
+for struct, mask, _ in qm.understood_structures:
+    if struct == problem_struct:
+        break
 
-# # %%
-# print(dataset[0][100:])
+print(f"struct: {struct} | mask: {mask}")
 
-# # %%
-# print(model.tokenizer.encode(model.tokenizer.decode(dataset[0, :303], skip_special_tokens=False), add_special_tokens=True))
+# print(struct, mask, problem_struct)
+# struct, mask, _ = qm.understood_structures[0]
+# reconfig_dset = qm.problem.reconfigure(dataset_subset, struct)
 
-# %%
-# print(
-#     model.generate(model.tokenizer.decode(dataset[0, :303]), max_new_tokens=101, stop_at_eos=False)
-# )
+preds, correct = qm.predict(model, dataset_subset, struct, mask)
+correct = correct.bool().item()
+print("correct:", correct)
 
-# # %%
-# for i in range(5):
-#     print(
-#         model.generate(
-#             "0 D D D 0 1 1 E",
-#             stop_at_eos=False,
-#             temperature=1,
-#             verbose=False,
-#             max_new_tokens=(404-8),
-#         )
-#     )
+if not correct:
+    print("Ground Truth")
+    print(repr_grid(dataset_subset[0]))
+    print("################################################################################")
 
-# %%
+print(repr_grid(preds[0]))
