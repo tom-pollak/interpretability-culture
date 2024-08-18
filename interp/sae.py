05e7241a2518dc718caf37438a4be329491ab966
Tom Pollak <tompollak1000@gmail.com>
2024-08-18 12:25:36 +0100

    udpate

--- interp/sae.py
+++ interp/sae.py
@@ -17,14 +17,9 @@ model = model.eval().to(device)
 sae = SAE.load_from_pretrained(str(sae_pretrained_path), device=str(device))
 
 
-dataset = load_dataset("tommyp111/culture-puzzles-1M", split="train")
-assert isinstance(dataset, Dataset)
+dataset = load_dataset("tommyp111/culture-puzzles-1M-partitioned")
+assert isinstance(dataset, DatasetDict)
 dataset.set_format("pt")
-
-
-# %%
-partitioned_datasets.push_to_hub("culture-puzzles-1M-partitioned")
-
 
 # %%
 
@@ -39,5 +34,55 @@ def quiz_diff_mask(quizzes):
         quizzes[:, final_grid_slice] != quizzes[:, second_final_grid_slice]
     )
 
+def zero_abl_hook(activation, hook):
+    return t.zeros_like(activation)
 
-pd["frame"]
+# %%
+
+# batch = dataset["frame"]["input_ids"][:100].to(device) # shave off last token
+for label, data in dataset.items():
+    print("task:", label)
+    batch = data["input_ids"][:100].to(device)
+    batch_stripped = batch[:, :-1]
+    # mask = quiz_diff_mask(batch_stripped)
+    with t.inference_mode():
+        model.reset_hooks()
+        orig_logits, orig_loss = model(batch_stripped, return_type="both", loss_per_token=True)
+
+        # orig_tfm_loss = orig_loss[mask].mean().item()
+        # orig_tfm_logits = orig_loss[mask].mean().item()
+        orig_tfm_loss = orig_loss.mean().item()
+        orig_tfm_logits = orig_loss.mean().item()
+
+        print(f"orig loss: {orig_tfm_loss:.2e}")
+        for i in [0, 8]: # range(model.cfg.n_layers):
+            logits, loss = model.run_with_hooks(
+                batch_stripped,
+                return_type="both",
+                fwd_hooks=[(f"blocks.{i}.hook_mlp_out", zero_abl_hook)],
+                loss_per_token=True,
+            )
+            # tfm_loss = loss[mask].mean().item()
+            # tfm_logits = logits[mask]
+            tfm_loss = loss.mean().item()
+            tfm_logits = logits
+            print(f"Layer {i} diff: {tfm_loss - orig_tfm_loss:.2e}")
+
+# %%
+orig_correct, orig_pred = generate(model, batch[:100])
+
+model.add_hook("blocks.8.hook_mlp_out", zero_abl_hook)  # type: ignore
+abl_correct, abl_pred = generate(model, batch[:100], verbose=False, prefix_0=False)
+model.reset_hooks()
+
+
+# %%
+model.add_hook("blocks.8.hook_mlp_out", zero_abl_hook)  # type: ignore
+generate_and_print(model, batch[:1], prefix_0=False)
+model.reset_hooks()
+# %%
+
+"""
+Using train_sae.py, trained a model (somewhat arbitarily)
+
+"""
